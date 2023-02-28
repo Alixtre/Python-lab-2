64c6c3be1d52577ad89003df183ed42c1be90ff6
Alixtre <zerskr0@gmail.com>
2023-02-28 16:44:21 +0200

    First commit

---
+++ lab_2_Akhinian.py
@@ -0,0 +1,18 @@
+# Завдання 1
+print("Programming", "Essential", "in", sep="***", end="...")
+print("Python")
+
+# Завдання 2
+print("    *\n   * *\n  *   *\n *     *\n***   ***\n  *   *\n  *   *\n  *****")
+
+# Завдання 3
+print("I'm student")
+
+# Завдання 4
+print("\"I'm\"\n\"\"learning\"\"\n\"\"\"Python\"\"\"")
+
+# Завдання 5
+print(0o500)
+
+# Завдання 6
+print(0x777)
